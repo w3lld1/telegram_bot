69ac6db7a54d7d7bf017034a985d1863a380fddc
Nikulin Nikita <faststepbyme@gmail.com>
2023-03-24 22:51:16 +0300

    changed bot logic

--- script.py
+++ script.py
@@ -28,27 +28,31 @@ def send_meme(chat_id):
     else:
         bot.send_message(chat_id=chat_id, text='Failed to fetch a meme. Try again later.')
 
+def send_meme_with_delay(chat_id, delay):
+    while True:
+        send_meme(chat_id)
+        time.sleep(delay)  # Wait for 3 hours (10800 seconds)
+
 @bot.message_handler(commands=['meme'])
 def handle_meme_command(message):
     send_meme(message.chat.id)
 
 @bot.message_handler(commands=['setinterval'])
 def handle_set_interval_command(message):
-    global interval
     try:
         new_interval = int(message.text.split()[1])
         interval = new_interval * 60  # Convert to seconds
+        send_meme_with_delay(message.chat.id, interval)
         bot.reply_to(message, f'Successfully set the interval to {new_interval} minutes.')
     except (IndexError, ValueError):
         bot.reply_to(message, 'Usage: /setinterval <minutes>')
+
 
 @bot.message_handler(commands=['start'])
 def handle_start_command(message):
     bot.reply_to(message, 'Bot started! Sending memes every 3 hours.')
 
-    while True:
-        send_meme(message.chat.id)
-        time.sleep(interval)  # Wait for 3 hours (10800 seconds)
+    send_meme_with_delay(message.chat.id, interval)
 
 
 bot.polling(none_stop=True)
