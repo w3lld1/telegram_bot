a5d71756807c4df31ede462626b46d71ec808c97
Nikulin Nikita <faststepbyme@gmail.com>
2023-03-24 22:36:03 +0300

    Initial commit

---
+++ script.py
@@ -0,0 +1,54 @@
+import time
+import requests
+import telebot
+from telebot import types
+
+BOT_TOKEN = '5785560958:AAFlufrWRoSbfQk09ZPHvyHuLBUNtZsUCAE'
+DEFAULT_INTERVAL = 10800  # 3 hours in seconds
+interval = DEFAULT_INTERVAL
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+def get_random_meme():
+    try:
+        url = 'https://meme-api.com/gimme'
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = response.json()
+            return data['url']
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+    return None
+
+def send_meme(chat_id):
+    meme_url = get_random_meme()
+    if meme_url:
+        bot.send_photo(chat_id=chat_id, photo=meme_url)
+    else:
+        bot.send_message(chat_id=chat_id, text='Failed to fetch a meme. Try again later.')
+
+@bot.message_handler(commands=['meme'])
+def handle_meme_command(message):
+    send_meme(message.chat.id)
+
+@bot.message_handler(commands=['setinterval'])
+def handle_set_interval_command(message):
+    global interval
+    try:
+        new_interval = int(message.text.split()[1])
+        interval = new_interval * 60  # Convert to seconds
+        bot.reply_to(message, f'Successfully set the interval to {new_interval} minutes.')
+    except (IndexError, ValueError):
+        bot.reply_to(message, 'Usage: /setinterval <minutes>')
+
+@bot.message_handler(commands=['start'])
+def handle_start_command(message):
+    bot.reply_to(message, 'Bot started! Sending memes every 3 hours.')
+
+    while True:
+        send_meme(message.chat.id)
+        time.sleep(interval)  # Wait for 3 hours (10800 seconds)
+
+
+bot.polling(none_stop=True)
